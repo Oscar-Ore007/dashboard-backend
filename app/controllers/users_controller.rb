bb1415ac50c264ae2ac4f6990da12017b2e4c106
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-15 12:18:43 -0400

    index,show, and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,49 @@
 class UsersController < ApplicationController
     attr_accessor :passsword
 
-    def authenticate(plaintext_passwprd)
-      if BCrypt::Password.new(self.password_digest) == plaintext_passwprd
-        self 
-      else
-        false
-      end
-    end
+    # def authenticate(plaintext_passwprd)
+    #   if BCrypt::Password.new(self.password_digest) == plaintext_passwprd
+    #     self 
+    #   else
+    #     false
+    #   end
+    # end
+
+    def index 
+        @users = User.all
+        render json: @users 
+    end 
+
+    def show 
+        @user = User.find(params[:id])
+
+        render json: @user 
+    end 
+
+
   
     def create 
-      @user = User.create(user_params)
+        get_users_params 
+            
+        @user = User.create(@user_params)
       if user.valid?
         render json: { user: UserSerializer.new(@user) }, status: :created
       else
-        render json: { error: params.errors.full_messages }, status: :not_acceptable
+        render json: { error: @user.errors.full_messages }, status: :not_acceptable
       end
     end
+
+    def authenticate(plaintext_passwprd)
+        if BCrypt::Password.new(self.password_digest) == plaintext_passwprd
+            self
+        else 
+            false 
+        end 
+    end 
   
     private
-    def user_params
-      params.require(:user).permit(:name, :username, :email, :password)
+    def get_user_params
+        params.permit(:name, :username)
+        @user_params = { name: params[:name], username: params[:username]}
     end
   end
