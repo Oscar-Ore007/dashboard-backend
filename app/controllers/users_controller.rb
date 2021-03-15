e9ef3ff0dfb8a9c945762bf74cf903072a526764
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-15 12:10:24 -0400

    created user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
-end
+    attr_accessor :passsword
+
+    def authenticate(plaintext_passwprd)
+      if BCrypt::Password.new(self.password_digest) == plaintext_passwprd
+        self 
+      else
+        false
+      end
+    end
+  
+    def create 
+      @user = User.create(user_params)
+      if user.valid?
+        render json: { user: UserSerializer.new(@user) }, status: :created
+      else
+        render json: { error: params.errors.full_messages }, status: :not_acceptable
+      end
+    end
+  
+    private
+    def user_params
+      params.require(:user).permit(:name, :username, :email, :password)
+    end
+  end
