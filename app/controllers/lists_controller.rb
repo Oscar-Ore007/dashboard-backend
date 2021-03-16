08e67cfb4e57a68556e869756e59d71d4e8bb42f
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-16 09:59:15 -0400

    updated List Controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,33 @@
 class ListsController < ApplicationController
-    belongs_to :project 
+    def index 
+        @lists = List.all 
+
+        render json: ListSerializer.new(@lists)
+    end 
+
+    def show 
+        @list = List.find(params[:id])
+        render json: @list 
+    end 
+
+    def create 
+        @list = List.create(list_params)
+
+        if @list.valid?
+            render json: @list 
+        else 
+            render json: { errors: @list.errors.full_messages }, status: :not_acceptable
+        end
+    end 
+
+    def delete 
+        @list = List.find(params[:id])
+        @list.destroy 
+    end 
+
+    private 
+
+    def list_params 
+        params.permit(:name, :project_id)
+    end 
 end
